72cbc7013a4bd1d0e2d408ae3a22ec06a2422868
Guilherme <143561085+Guilhermeb166@users.noreply.github.com>
2025-11-16 23:37:55 -0300

    Trocando o modelo de chat, para outra versão

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -57,7 +57,7 @@ try {  const { prompt } = await req.json();
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      model: 'meta-llama/llama-3.3-8b-instruct:free',
+      model: 'meta-llama/llama-3.3-70b-instruct:free',
       messages: [
         { role: 'system', content: context },
         { role: 'user', content: prompt },
